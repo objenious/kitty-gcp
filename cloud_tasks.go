34901bd78e56bffd81533863068df77fbd8d0991
Baptiste Boussemart <bboussemart.ext@objenious.com>
2018-08-29 15:02:23 +0200

    initial prototype

---
+++ cloud_tasks.go
@@ -0,0 +1,249 @@
+package kittygcp
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/duration"
+
+	"objenious/lib/log"
+
+	"github.com/go-kit/kit/endpoint"
+
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2beta2"
+	"github.com/objenious/kitty"
+	"github.com/pkg/errors"
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2beta2"
+)
+
+// CloudTasksClient is the Cloud Tasks client
+type CloudTasksClient struct {
+	googleCloudTasksClient *cloudtasks.Client
+	closed                 bool
+	endpoints              []*cloudtaskendpoint
+}
+
+var _ kitty.Transport = &CloudTasksClient{}
+
+// NewCloudTasksClient creates a new Cloud Tasks client.
+func NewCloudTasksClient() *CloudTasksClient {
+	return &CloudTasksClient{}
+}
+
+type cloudtaskendpoint struct {
+	queueName          string
+	endpoint           endpoint.Endpoint
+	maxTasks           int32
+	leaseTimeInSeconds int64
+	decode             func([]byte) (interface{}, error)
+}
+
+// Start starts pulling msg from Cloud Tasks.
+func (ctc *CloudTasksClient) Start(ctx context.Context) error {
+	logger := log.LoggerFromContext(ctx)
+	var err error
+	ctc.googleCloudTasksClient, err = cloudtasks.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	ticker := time.NewTicker(time.Second)
+	for {
+		if ctc.closed {
+			return nil
+		}
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			for _, e := range ctc.endpoints {
+				msgs, err := ctc.leaseTasks(ctx, e)
+				if err != nil {
+					logger.Err(log.LevelError, err, "get Cloud Tasks msg")
+					break
+				}
+				for i := range msgs {
+					ctc.process(ctx, e, msgs[i])
+				}
+			}
+		}
+	}
+}
+
+// Shutdown shutdowns the cloud tasks client
+func (ctc *CloudTasksClient) Shutdown(ctx context.Context) error {
+	ctc.closed = true
+	return ctc.googleCloudTasksClient.Close()
+}
+
+// RegisterEndpoints register endpoint
+func (ctc *CloudTasksClient) RegisterEndpoints(m endpoint.Middleware, fn kitty.AddLoggerToContextFn) error {
+	for _, e := range ctc.endpoints {
+		m(e.endpoint)
+	}
+	return nil
+}
+
+// Endpoint registers endpoint
+func (ctc *CloudTasksClient) Endpoint(queueName string, maxTasks int32, leaseTimeInSecond int64, ep endpoint.Endpoint, decode func([]byte) (interface{}, error)) *CloudTasksClient {
+	e := &cloudtaskendpoint{
+		maxTasks:           maxTasks,
+		leaseTimeInSeconds: leaseTimeInSecond,
+		queueName:          queueName,
+		endpoint:           ep,
+		decode:             decode,
+	}
+	ctc.endpoints = append(ctc.endpoints, e)
+	return ctc
+}
+
+func (ctc *CloudTasksClient) process(ctx context.Context, e *cloudtaskendpoint, msg *taskspb.Task) {
+
+	logger := log.New("task_id", msg.Name)
+	ctx = log.ContextWithLogger(ctx, logger)
+
+	defer func() {
+		if r := recover(); r != nil {
+			if err, ok := r.(error); ok {
+				logger.Errf(log.LevelCrit, err, "FatalError error (nack)")
+			} else {
+				logger.Errf(log.LevelError, errors.Errorf("%v", r), "FatalError error (nack)")
+			}
+
+			err := ctc.nack(ctx, msg)
+			if err != nil {
+				logger.Errf(log.LevelError, err, "FatalError error (nack) : inside recover")
+			}
+		}
+	}()
+	start := time.Now()
+	pm := msg.GetPullMessage()
+	data := pm.GetPayload()
+	var err error
+	for _, e := range ctc.endpoints {
+		s, er := e.decode(data)
+		if er != nil {
+			err = er
+			break
+		}
+		_, er = e.endpoint(ctx, s)
+		if er != nil {
+			err = er
+			break
+		}
+	}
+	if err != nil {
+		delay := getDelay(err)
+		if delay == 0 {
+			logger.Debug("nack")
+			err = ctc.nack(ctx, msg)
+			if err != nil {
+				logger.Errf(log.LevelCrit, err, "FatalError error (nack)")
+			}
+		} else {
+			logger.Debug("nack delay")
+			err = ctc.nackWithDelay(ctx, msg, delay)
+			if err != nil {
+				logger.Errf(log.LevelCrit, err, "FatalError error (nack with delay)")
+			}
+		}
+	} else {
+		err = ctc.ack(ctx, msg)
+		if err != nil {
+			logger.Errf(log.LevelCrit, err, "FatalError error (ack)")
+		}
+	}
+	logger.Debugf("Request processed in %s", time.Since(start))
+	if err != nil {
+		logger.Err(log.LevelWarn, err, "Error processing request")
+	}
+}
+
+func (ctc *CloudTasksClient) leaseTasks(ctx context.Context, e *cloudtaskendpoint) ([]*taskspb.Task, error) {
+	tasksResp, err := ctc.googleCloudTasksClient.LeaseTasks(ctx, &taskspb.LeaseTasksRequest{
+		Parent:   e.queueName,
+		MaxTasks: e.maxTasks,
+		LeaseDuration: &duration.Duration{
+			Seconds: e.leaseTimeInSeconds,
+		},
+		ResponseView: taskspb.Task_FULL,
+	})
+	return tasksResp.Tasks, errors.Wrapf(err, "getting messages from %s", e.queueName)
+}
+
+func (ctc *CloudTasksClient) ack(ctx context.Context, t *taskspb.Task) (err error) {
+	err = ctc.googleCloudTasksClient.AcknowledgeTask(ctx, &taskspb.AcknowledgeTaskRequest{
+		Name:         t.Name,
+		ScheduleTime: t.ScheduleTime,
+	})
+	return errors.Wrap(err, "ack")
+}
+
+func (ctc *CloudTasksClient) nack(ctx context.Context, t *taskspb.Task) (err error) {
+	_, err = ctc.googleCloudTasksClient.CancelLease(ctx, &taskspb.CancelLeaseRequest{
+		Name:         t.Name,
+		ScheduleTime: t.ScheduleTime, // this is required to make sure that our worker hold the lease
+	})
+	return errors.Wrap(err, "nack")
+}
+
+// nackWithDelay extends the lease until it expires, so a worker can take it back to create a delayed Nack
+func (ctc *CloudTasksClient) nackWithDelay(ctx context.Context, t *taskspb.Task, delay time.Duration) (err error) {
+	if delay < time.Second {
+		delay = time.Second
+	}
+
+	_, err = ctc.googleCloudTasksClient.RenewLease(ctx, &taskspb.RenewLeaseRequest{
+		Name:         t.Name,
+		ScheduleTime: t.ScheduleTime,
+		LeaseDuration: &duration.Duration{
+			Seconds: int64(delay.Seconds()),
+		},
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "nack delay")
+	}
+
+	log.LoggerFromContext(ctx).Debug("nack with delay %s", delay.String())
+
+	return nil
+}
+
+// delayabler defines an error with a retry delay
+type delayabler interface {
+	Delay() time.Duration
+}
+
+type delaybleErr struct {
+	error
+	delay time.Duration
+}
+
+func delaybleError(err error, delay time.Duration) error {
+	return &delaybleErr{
+		error: err,
+		delay: delay,
+	}
+}
+
+func (err *delaybleErr) Delay() time.Duration {
+	return err.delay
+}
+
+func getDelay(err error) time.Duration {
+	type causer interface {
+		Cause() error
+	}
+
+	for err != nil {
+		if retry, ok := err.(delayabler); ok {
+			return retry.Delay()
+		}
+		cause, ok := err.(causer)
+		if !ok {
+			break
+		}
+		err = cause.Cause()
+	}
+	return 0
+}
---
+++ cloud_tasks_test.go
@@ -0,0 +1,161 @@
+package kittygcp
+
+import (
+	"context"
+	"encoding/json"
+	"reflect"
+	"testing"
+	"time"
+
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2beta2"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/objenious/kitty"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2beta2"
+)
+
+type msgTest struct {
+	Msg string `json:"msg"`
+}
+
+var (
+	testCh = make(chan interface{})
+)
+
+func testEP(_ context.Context, req interface{}) (interface{}, error) {
+	if r, ok := req.(*testStruct); ok && r.Status != 0 {
+		return nil, errors.Errorf("status=%v", r.Status)
+	}
+	testCh <- req
+	return req, nil
+}
+
+const queueName = "projects/objenious-dev/locations/us-central1/queues/test-queue"
+
+func TestServer(t *testing.T) {
+	shutdownCalled := false
+	ctx, cancel := context.WithCancel(context.Background())
+	exitError := make(chan error)
+	tr := NewCloudTasksClient().
+		Endpoint(queueName, 1, 60, testEP, decode)
+	srv := kitty.NewServer(tr).Shutdown(func() {
+		shutdownCalled = true
+	})
+	go func() {
+		exitError <- srv.Run(ctx)
+	}()
+	for tr.googleCloudTasksClient == nil {
+		time.Sleep(time.Millisecond)
+	}
+	{
+		_, err := send(ctx, queueName, tr, []byte(`{"foo":"bar"}`), time.Millisecond)
+		if err != nil {
+			t.Errorf("send to cloud tasks : %s", err)
+		} else {
+			resData := <-testCh
+			if !reflect.DeepEqual(resData, &testStruct{Foo: "bar", Status: 0}) {
+				t.Errorf("cloud tasks returned invalid data : %+v", resData)
+			}
+		}
+	}
+	cancel()
+	select {
+	case <-time.After(time.Second):
+		t.Error("Server.Run has not stopped after 1sec")
+	case err := <-exitError:
+		if err != nil && err != context.Canceled {
+			t.Errorf("Server.Run returned an error : %s", err)
+		}
+	}
+	if !shutdownCalled {
+		t.Error("Shutdown functions are not called")
+	}
+}
+
+type testStruct struct {
+	Foo    string `json:"foo"`
+	Status int    `json:"status"`
+}
+
+func decode(b []byte) (interface{}, error) {
+	d := &testStruct{}
+	err := json.Unmarshal(b, d)
+	if err != nil {
+		return nil, err
+	}
+	return d, nil
+}
+
+func TestCloudTasksClient(t *testing.T) {
+	t.Log("I want to test the Cloud Tasks client")
+	{
+		t.Log("Given a Cloud Tasks client")
+		ctx := context.TODO()
+		ch := make(chan interface{})
+		ctClient := NewCloudTasksClient().
+			Endpoint(queueName, 1, 60, func(ctx context.Context, r interface{}) (interface{}, error) {
+				ch <- r
+				return nil, nil
+			}, decode)
+
+		t.Log("when I send a message")
+		msg := msgTest{Msg: "hello"}
+		data, err := json.Marshal(msg)
+		assert.NoError(t, err)
+		ctClient.googleCloudTasksClient, err = cloudtasks.NewClient(ctx)
+		_, err = send(ctx, queueName, ctClient, data, 1)
+
+		t.Log("Then, I should not get any error")
+		assert.NoError(t, err)
+
+		t.Log("And I should be able to read it")
+		var c = make(chan bool, 1)
+
+		go func() {
+			ctClient.RegisterEndpoints(func(e endpoint.Endpoint) endpoint.Endpoint {
+				MsgRead := &msgTest{}
+				err := json.Unmarshal(data, MsgRead)
+				assert.NoError(t, err)
+				assert.Equal(t, "hello", MsgRead.Msg)
+				c <- true // listen done => terminate test
+				return nil
+			}, nil)
+			ctClient.Start(ctx)
+		}()
+
+		// In case of the message is not received, we wait 10 seconds max.
+		timer := time.After(10 * time.Second)
+		select {
+		case <-c:
+			time.Sleep(2 * time.Second) // wait for ack to be done
+			// it worked
+		case <-timer:
+			// it did not
+			t.Fatal("Message sent to cloud tasks not received")
+		}
+		ctx.Done()
+	}
+}
+
+// Send sends a message to a Cloud Tasks queue. The queue must already exist.
+func send(ctx context.Context, QueueName string, ctc *CloudTasksClient, data []byte, delay time.Duration) (*taskspb.Task, error) {
+	schTime := time.Now().Add(delay)
+	timestamp := timestamp.Timestamp{
+		Seconds: schTime.Unix(),
+	}
+	req := &taskspb.CreateTaskRequest{
+		Parent: queueName,
+		Task: &taskspb.Task{
+			ScheduleTime: &timestamp,
+			PayloadType: &taskspb.Task_PullMessage{
+				PullMessage: &taskspb.PullMessage{
+					Payload: data,
+				},
+			},
+		},
+	}
+	resp, err := ctc.googleCloudTasksClient.CreateTask(ctx, req)
+	return resp, errors.Wrap(err, "create task")
+}
