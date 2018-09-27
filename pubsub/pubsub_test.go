f5d54bc003d316daee92ff74fa1b6fb5b9d484bf
Baptiste Boussemart <bboussemart.ext@objenious.com>
2018-09-27 15:16:03 +0200

    use first pub/sub client for the test

--- pubsub/pubsub_test.go
+++ pubsub/pubsub_test.go
@@ -93,6 +93,7 @@ func TestPubSubWithMultipleEndpoints(t *testing.T) {
 		t.Fatal(err)
 		return
 	}
+	defer c.Close()
 
 	err = createTopicAndSubscribtion(ctx, c, topic1Name, sub1Name)
 	if err != nil {
@@ -106,30 +107,33 @@ func TestPubSubWithMultipleEndpoints(t *testing.T) {
 		return
 	}
 
-	err = c.Close()
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-
 	shutdownCalled := false
 	ctx, cancel := context.WithCancel(context.Background())
-	exitError := make(chan error)
+
 	tr := NewTransport(ctx, "project").
 		Endpoint(sub1Name, testEP, Decoder(decode)).
 		Endpoint(sub2Name, testEP, Decoder(decode))
 	srv := kitty.NewServer(tr).Shutdown(func() {
 		shutdownCalled = true
 	})
+
+	exitError := make(chan error)
 	go func() {
 		exitError <- srv.Run(ctx)
 	}()
-	for tr.c == nil {
-		time.Sleep(time.Millisecond)
+
+	for start := time.Now(); tr.c == nil && time.Since(start) < time.Minute; {
+		// waits until pub/sub client is created in the transport, max 1 minute
+		time.Sleep(time.Millisecond) // avoids too much CPU
+	}
+	if tr.c == nil {
+		t.Error("after 1 minute, the pub/sub client is still not initialized")
+		cancel()
+		return
 	}
 
-	sendThenReceiveWithTimeout(ctx, t, tr.c, topic1Name)
-	sendThenReceiveWithTimeout(ctx, t, tr.c, topic2Name)
+	sendThenReceiveWithTimeout(ctx, t, c, topic1Name)
+	sendThenReceiveWithTimeout(ctx, t, c, topic2Name)
 
 	cancel()
 	select {
@@ -184,7 +188,7 @@ func decode(ctx context.Context, b []byte) (interface{}, error) {
 	return d, nil
 }
 
-// send sends a message to a Cloud Tasks queue. The queue must already exist.
+// send sends a message to Pub/Sub topic. The topic must already exist.
 func send(ctx context.Context, topic string, c *pubsub.Client, data []byte) error {
 	t := c.Topic(topic)
 	res := t.Publish(ctx, &pubsub.Message{Data: data})
