900ddafbec76e2174de0c5829c1db25363d01b6f
Baptiste Boussemart <bboussemart.ext@objenious.com>
2018-10-15 17:21:29 +0200

    remove testify

--- pubsub/client_test.go
+++ pubsub/client_test.go
@@ -6,46 +6,64 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	ps "cloud.google.com/go/pubsub"
+	"cloud.google.com/go/pubsub"
 )
 
 func TestClient(t *testing.T) {
 	t.Log("pubsub setup")
 	ctx := context.TODO()
-	psc, err := ps.NewClient(ctx, "project")
-	require.NoError(t, err)
+
+	psc, err := pubsub.NewClient(ctx, "project")
+	if err != nil {
+		t.Error("pubsub new client", err)
+	}
+
 	topic, err := psc.CreateTopic(ctx, "testclienttopic")
-	require.NoError(t, err)
-	sub, err := psc.CreateSubscription(ctx, "testclientsub", ps.SubscriptionConfig{
+	if err != nil {
+		t.Error("create topic", err)
+	}
+
+	sub, err := psc.CreateSubscription(ctx, "testclientsub", pubsub.SubscriptionConfig{
 		Topic: topic,
 	})
-	require.NoError(t, err)
-	ch := make(chan *ps.Message, 1)
+	if err != nil {
+		t.Error("create subscription", err)
+	}
+
+	ch := make(chan *pubsub.Message, 1)
 	go func() {
-		sub.Receive(ctx, func(ctx context.Context, msg *ps.Message) {
+		sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
 			ch <- msg
 			msg.Ack()
 		})
 	}()
 
 	t.Log("create client")
+
 	c, err := NewClient(ctx, "project", topic.ID())
-	require.NoError(t, err)
+	if err != nil {
+		t.Error("new client under test", err)
+	}
+
 	endp := c.Endpoint()
 
 	t.Log("send data with the client, test reception")
 	timer := time.NewTimer(10 * time.Second)
 	for i := 0; i < 10; i++ {
 		_, err = endp(ctx, fmt.Sprintf("hello world! #%d", i))
-		assert.NoError(t, err)
+		if err != nil {
+			t.Error("client endpoint", err)
+		}
+
 		timer.Reset(10 * time.Second)
 		select {
 		case m := <-ch:
-			assert.NotNil(t, m)
-			assert.Equal(t, fmt.Sprintf(`"hello world! #%d"`, i), string(m.Data))
+			if m == nil {
+				t.Error("no message found", err)
+			}
+			if fmt.Sprintf(`"hello world! #%d"`, i) != string(m.Data) {
+				t.Error("message expected is different", err)
+			}
 		case <-timer.C:
 			t.Error("timeout reached")
 		}
